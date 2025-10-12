57bf26ab3b6c84084b478f7a2b42d335a515da1a
KaushaLPanchaL24 <kaushalpanchal002@gmail.com>
2025-10-12 23:29:20 +0530

    Added star pattern printing program in Java

--- Patterns/print_star_pattern.java
+++ Patterns/print_star_pattern.java
@@ -1,18 +1,28 @@
+/* This program prints a left-aligned triangle star pattern */
+
+
+// import scanner class
 import java.util.Scanner;
 
 public class print_star_pattern {
     public static void main(String[] args){
 
+        // make scanner object
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter line you want to print pateern : ");
+        // ask the user for input
+        System.out.println("Enter the number of lines for the pattern : ");
         int number_of_line = sc.nextInt();
 
+        // outer loop for number of lines
         for(int line = 1; line <= number_of_line; line++){
 
+            // inner loop to print stars in each line
             for (int star = 1; star <= line; star++){
                 System.out.print("*");
             }
+
+            // move to the next line after printing stars
             System.out.println();
         }
     }
